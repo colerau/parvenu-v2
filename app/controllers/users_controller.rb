1a2877cf0e8e7aecb58eabb5454ddc0acb68c527
Cole Rau <colevrau@gmail.com>
2020-07-09 19:58:25 -0700

    initial code for user sign up; worked on users controller, users new view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+    def new 
+        @user = User.new
+    end
+
+    def create 
+        @user = User.new(user_params)
+
+        @user.email.downcase!
+
+        if @user.save
+            flash[:notice] = "You're in."
+            redirect_to root_path
+        end
+    end
+
+    def destroy 
+
+    end
+
+    private 
+
+    def user_params 
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
