7420a89dc198586d54f720dc879af48497c2b1ea
Cole Rau <colevrau@gmail.com>
2020-07-09 23:12:12 -0700

    flash[:notice] now shows up in browser; view code for user login

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'users#new'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 end
