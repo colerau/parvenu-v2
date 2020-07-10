c1d3260200e215ed11537b90cde567fff6f49f32
Cole Rau <colevrau@gmail.com>
2020-07-09 23:43:20 -0700

    user can log in when credentials correct, user can't log in when credentials incorrect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,17 @@ class SessionsController < ApplicationController
 
     end
 
-    def create 
-        binding.pry
+    def create
+        user = User.find_by(email: params[:email].downcase)
+
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id 
+            flash[:notice] = "Log In Successful"
+            redirect_to root_path
+        else 
+            flash[:notice] = "Could not find account :("
+            render :new 
+        end
     end
 
     def destroy 
